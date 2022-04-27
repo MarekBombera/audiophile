002be664da729c01fac33cd33b86e88beae0de70
Marek Bombera <bomber.marek@gmail.com>
2022-04-27 18:18:58 +0200

    update styled components setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ import { ServerStyleSheet } from 'styled-components';
 import { Fragment } from 'react';
 import { RenderPageResult } from 'next/dist/shared/lib/utils';
 
-export declare type DocumentInitialProps = RenderPageResult & {
+declare type DocumentInitialProps = RenderPageResult & {
 	styles?: React.ReactElement[] | React.ReactFragment;
 };
 
@@ -31,10 +31,10 @@ export default class MyDocument extends Document {
 			return {
 				...initialProps,
 				styles: [
-					<>
+					<Fragment key="1">
 						{initialProps.styles}
 						{sheet.getStyleElement()}
-					</>,
+					</Fragment>,
 				],
 			};
 		} finally {
@@ -42,42 +42,3 @@ export default class MyDocument extends Document {
 		}
 	}
 }
-
-// export default class MyDocument extends Document {
-// 	static async getInitialProps(ctx: DocumentContext) {
-// 		const sheet = new ServerStyleSheet();
-// 		const originalRenderPage = ctx.renderPage;
-
-// 		try {
-// 			ctx.renderPage = () =>
-// 				originalRenderPage({
-// 					enhanceApp: (App) => (props) =>
-// 						sheet.collectStyles(<App {...props} />),
-// 				});
-
-// 			const initialProps = await Document.getInitialProps(ctx);
-// 			return {
-// 				...initialProps,
-// 				styles: [
-// 					<Fragment key="1">
-// 						{initialProps.styles}
-// 						{sheet.getStyleElement()}
-// 					</Fragment>,
-// 				],
-// 			};
-// 		} finally {
-// 			sheet.seal();
-// 		}
-// 	}
-// 	render() {
-// 		return (
-// 			<Html>
-// 				<Head />
-// 				<body>
-// 					<Main />
-// 					<NextScript />
-// 				</body>
-// 			</Html>
-// 		);
-// 	}
-// }
