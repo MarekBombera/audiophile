195275c9ef154ca8b9568aa31eecf1a465874405
Marek Bombera <bomber.marek@gmail.com>
2022-04-27 19:19:38 +0200

    changed ’ to '

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -42,10 +42,10 @@ export const Footer = (): JSX.Element => {
 			</FooterNav>
 			<About>
 				<p>
-					Audiophile is an all in one stop to fulfill your audio needs. We're a
+					{`Audiophile is an all in one stop to fulfill your audio needs. We're a
 					small team of music lovers and sound specialists who are devoted to
 					helping you get the most out of personal audio. Come and visit our
-					demo facility - we're open 7 days a week.
+					demo facility - we're open 7 days a week.`}
 				</p>
 			</About>
 			<Copyright>
